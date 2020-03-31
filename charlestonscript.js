fdd7ac9689155a37a7c70397ea4e470b77243133
esargen <60758777+esargen@users.noreply.github.com>
2020-03-31 07:24:42 -0400

    scripty

---
+++ charlestonscript.js
@@ -0,0 +1,25 @@
+
+function openNav() {
+  document.getElementById("itinerary").style.height = "425px";
+  document.getElementById("closebtn").style.opacity = "1.0";
+}
+function closeNav() {
+  document.getElementById("itinerary").style.height = "30px";
+  document.getElementById("closebtn").style.opacity = "0.0";
+}
+function blueTrans() {
+  document.getElementById("landing").style.height = "100%";
+  document.getElementById("title").style.opacity = "0%";
+  document.getElementById("subtitle").style.opacity = "0%";
+  document.getElementById("navbar").style.opacity = "0%";
+}
+function sideNav() {
+  document.getElementById("sidenav").style.width = "100%";
+  document.getElementById("sidenav").style.opacity = "0.6";
+  document.getElementById("closenav").style.opacity = "1.0";
+}
+function closeSideNav() {
+  document.getElementById("sidenav").style.width = "0%";
+  document.getElementById("sidenav").style.opacity = "0.0";
+  document.getElementById("closenav").style.opacity = "0.0";
+}
